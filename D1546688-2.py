a3a9b1a929f07bca7b4534e7bb6fefe7c019ad9d
Skylar <poskylarsj@gmail.com>
2026-09-17 10:49:45 +0800

    code

---
+++ D1546688-2.py
@@ -0,0 +1,11 @@
+n = input ("Enter a number: ")
+
+for i in range(int(n), 0, -1):
+    print ("*" * i)
+
+'''
+****
+***
+**
+*
+'''
---
+++ D1546688.py
@@ -0,0 +1,12 @@
+n = input ("Enter a number: ")
+
+for i in range(1, int(n)+1):
+    print ("*" * i)
+
+
+'''
+*
+**
+***
+****
+'''
